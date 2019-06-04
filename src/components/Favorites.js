a28b89e8fa32f5a1dd39d6550bbcd45f93c8aec5
Vawi <valentin.winnen@orange.fr>
2019-06-04 12:28:41 +0200

    ajout avatar, changement avatar en cours

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,24 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import FilmList from './FilmList';
+import Avatar from './Avatar'
 import { connect } from 'react-redux'
 
 class Favorites extends React.Component {
 
     render() {
         return (
-            <FilmList
-                films={this.props.favoritesFilm}
-                navigation={this.props.navigation}
-                favoritesList={true}
-            />
+            <View style={styles.main_container}>
+                <View style={styles.avatar_container}>
+                    <Avatar/>
+                </View>
+                <FilmList
+                    films={this.props.favoritesFilm}
+                    navigation={this.props.navigation}
+                    favoritesList={true}
+                />
+            </View>
         )
     }
 
 }
 
 const styles = StyleSheet.create({
-
+    main_container: {
+        flex: 1
+    }, avatar_container: {
+        alignItems: 'center'
+    }
 })
 
 const mapStateToProps = state => {
